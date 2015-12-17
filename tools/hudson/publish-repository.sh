c16e0b7bffbaff73dc642dd79e38efaca5666d70
Yasser Aziza <yasser.aziza@gmail.com>
2015-12-17 22:44:16 +0100

    [releng] Add target platform definition for Eclipse Neon (4.6)
    Change-Id: I7511fec83a22b7a63f772c48825bf6e3f2ba75b7
    Signed-off-by: Yasser Aziza <yasser.aziza@gmail.com>
    Signed-off-by: Andreas Sewe <andreas.sewe@codetrails.com>

--- tools/hudson/publish-repository.sh
+++ tools/hudson/publish-repository.sh
@@ -1,32 +1,41 @@
 #! /bin/bash -e
 
 if [ -z "$1" ]; then
-    echo "No simultaneous release name (e.g., 'luna', 'mars') supplied."
+    echo "No release JDK (e.g., 'jdk-1.8.0-latest') supplied."
     exit 1
 else
-    SIMREL=$1
+    JDK=$1
 fi
 
 if [ -z "$2" ]; then
-    echo "No release name ('head', 'milestones', 'stable', 'simrel') supplied."
+    echo "No simultaneous release name (e.g., 'kepler', 'luna', 'mars') supplied."
     exit 2
 else
-    RELEASE=$2
+    SIMREL=$2
 fi
 
 if [ -z "$3" ]; then
-   echo "No download path (e.g., 'downloads.eclipse.org/recommenders/updates/head') supplied."
-   exit 3
+    echo "No repository name (e.g., 'main') supplied."
+    exit 3
 else
-   DOWNLOAD_PATH=$3
+    REPOSITORY=$3
 fi
 
-echo "Publishing '${RELEASE}' repository of '${SIMREL}' configuration at http://${DOWNLOAD_PATH}"
+if [ -z "$4" ]; then
+    echo "No download path (e.g., 'downloads.eclipse.org/recommenders/updates/stable') supplied."
+    exit 4
+else
+    DOWNLOAD_PATH=$4
+fi
+
+echo "Publishing '${REPOSITORY}' repository of '${JDK}'/'${SIMREL}' configuration at http://${DOWNLOAD_PATH}"
 echo
 
-SOURCE=${HUDSON_HOME}/jobs/${PROMOTED_JOB_NAME}/configurations/axis-simrel/${SIMREL}/builds/${PROMOTED_ID}/archive/repositories/${RELEASE}/target/repository
+SOURCE=${HUDSON_HOME}/jobs/${PROMOTED_JOB_NAME}/configurations/axis-jdk/${JDK}/axis-simrel/${SIMREL}/builds/${PROMOTED_ID}/archive/repositories/${REPOSITORY}/target/repository
 TARGET=/home/data/httpd/${DOWNLOAD_PATH}
 
 mkdir -p ${TARGET}
 rm -f -R ${TARGET}/*
 cp -v -R ${SOURCE}/* ${TARGET}
+
+echo
